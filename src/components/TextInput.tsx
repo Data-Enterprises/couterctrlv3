142e2051fa509a745bf9dac04a74b5010836d0c7
Stephen <stephenn649@gmail.com>
2025-11-13 08:44:26 -0600

    Sets up Password and Confirm password in UserForm.tsx to check if the entered passwords match and are strong

--- src/components/TextInput.tsx
+++ src/components/TextInput.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
-import Eye from "../svgs/Eye";
 import { useAppSelector } from "../hooks";
+import zxcvbn from "zxcvbn";
+import Eye from "../svgs/Eye";
 
 interface TextInputProps<T> {
   query: string;
@@ -33,19 +34,78 @@ const TextInput = <T,>({
 
   const showMsg = () => {
     if (name === "confirm_password" && query.length > 0) {
-      if (userInfo.password !== userInfo.confirm_password)
+      if (userInfo.password !== userInfo.confirm_password) {
         return "- Passwords do not match";
+      } else if (userInfo.password === userInfo.confirm_password) {
+        return "- Passwords match";
+      }
     }
 
     if (name === "password" && query.length > 0) {
+      const score = zxcvbn(query).score;
       // check password strength
+      switch (score) {
+        case 0:
+          return "Very Weak";
+        case 1:
+          return "Weak";
+        case 2:
+          return "Moderate";
+        case 3:
+          return "Strong";
+        case 4:
+          return "Very Strong";
+      }
+    }
+  };
+
+  const showPasswordStrength = (score: number) => {
+    switch (score) {
+      case 0:
+        return "w-0 ";
+      case 1:
+        return "w-1/4 bg-red-500";
+      case 2:
+        return "w-1/2 bg-yellow-500";
+      case 3:
+        return "w-3/4 bg-orange-500";
+      case 4:
+        return "w-full bg-green-500";
+      default:
+        return "w-0";
+    }
+  };
+
+  const showTextColor = () => {
+    if (name === "confirm_password" && query.length > 0) {
+      if (userInfo.password !== userInfo.confirm_password) {
+        return "text-orange-500";
+      } else if (userInfo.password === userInfo.confirm_password) {
+        return "text-emerald-500";
+      }
+    }
+
+    const score = zxcvbn(query).score;
+    switch (score) {
+      case 0:
+        return "text-content";
+      case 1:
+        return "text-red-500";
+      case 2:
+        return "text-yellow-500";
+      case 3:
+        return "text-orange-500";
+      case 4:
+        return "text-green-500";
+      default:
+        return "text-content";
     }
   };
 
   return (
     <div className="relative">
       <label htmlFor={name} className="text-[13px] font-medium ml-1 flex gap-1">
-        {title} <div className="text-orange-500">{showMsg()}</div>
+        {title} <div className={showTextColor()}>{showMsg()}</div>
       </label>
       <input
         name={name}
@@ -55,6 +115,13 @@ const TextInput = <T,>({
         className="basic-input focus:border bg-custom-white"
       />
       {type === "password" && <Eye onClick={handleEncryptionToggle} />}
+      {name === "password" ? (
+        <div className="h-1 w-[97%] mx-auto rounded-full bg-content/10 mt-1 ">
+          <div
+            className={`${showPasswordStrength(zxcvbn(query).score)} h-full`}
+          ></div>
+        </div>
+      ) : null}
     </div>
   );
 };
