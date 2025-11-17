00e7e0081ab91e32bb6e967014deab8732033e91
Stephen <stephenn649@gmail.com>
2025-11-17 17:28:23 -0600

    Makes NewPassword it's own component in forgot password. All three steps of the process work as expected, users with valid credentials can reset their forgotten passwords with the current architecture

--- src/pages/home/forgot/ForgotPassword.tsx
+++ src/pages/home/forgot/ForgotPassword.tsx
@@ -1,24 +1,15 @@
 import { useEffect, useState } from "react";
 import { useAppSelector, useAppDispatch } from "../../../hooks";
-import type { JsonError } from "../../../interfaces";
 import { setForgotPassword } from "../../../features/appSlice";
-import { useToast } from "../../../components/toasts/hooks/useToast";
-
-import { resetForgotPassword } from "../../../api/password";
-
-import {
-  setNewPassword,
-  resetForgotPasswordState,
-} from "../../../features/forgotPasswordSlice";
+import { resetForgotPasswordState } from "../../../features/forgotPasswordSlice";
 
 import Modal from "../../../components/Modal";
 import Carousel from "../../../components/Carousel";
-import TextInput from "../../../components/TextInput";
 import EmailVerify from "./EmailVerify";
 import SecurityAnswer from "./SecurityAnswer";
+import NewPassword from "./NewPassword";
 
 const ForgotPassword = () => {
-  const toast = useToast();
   const dispatch = useAppDispatch();
   const context = useAppSelector((state) => state.app);
   const forgot = useAppSelector((state) => state.forgotPassword);
@@ -30,30 +21,13 @@ const ForgotPassword = () => {
     } else if (forgot.index === 1) {
       setHeight("h-[190px]");
     } else if (forgot.index === 2) {
-      setHeight("h-[169px]");
+      setHeight("h-[177px]");
     }
   }, [forgot.index]);
 
-  const setPasswordText = (text: string) => {
-    dispatch(setNewPassword(text));
-  };
-
-  // For when the modal closes
   const onClose = () => {
     dispatch(setForgotPassword(false));
     dispatch(resetForgotPasswordState());
-  };
-
-  const submitNewPassword = () => {
-    resetForgotPassword(context.url, forgot.username, forgot.newPassword)
-      .then((resp) => {
-        const j = resp.data;
-        if (j.error === 0) {
-          toast.success(j.msg);
-          onClose();
-        }
-      })
-      .catch((err: JsonError) => toast.error(err.message));
   };
 
   return (
@@ -70,28 +44,7 @@ const ForgotPassword = () => {
       >
         <EmailVerify />
         <SecurityAnswer />
-
-        {/* Reset Password Section */}
-        <div className="h-[169px] px-2">
-          <div className="font-medium text-center">Reset Password</div>
-          <div className="text-center">
-            Once updated, sign in with your new password
-          </div>
-          <TextInput
-            query={forgot.newPassword}
-            name="forgot-password"
-            isSimple={true}
-            type="password"
-            title="Password"
-            setText={setPasswordText}
-          />
-          <button
-            className="btn-themeBlue w-full mt-4"
-            onClick={submitNewPassword}
-          >
-            Change Password
-          </button>
-        </div>
+        <NewPassword onClose={onClose} />
       </Carousel>
     </Modal>
   );
