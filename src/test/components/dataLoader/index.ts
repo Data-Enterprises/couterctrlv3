8803f4ce965b123b82a3211a098f05cd0d2ea181
Stephen <stephenn649@gmail.com>
2025-12-05 14:36:31 -0600

    Adds 100% code coverage for UserDataLoader

---
+++ src/test/components/dataLoader/index.ts
@@ -0,0 +1,162 @@
+export const JsonErrorResp = new Error("API request failed");
+
+export const userPrefsResp = {
+  data: {
+    error: 0,
+    success: true,
+    prefs: [
+      {
+        userid: 1,
+        last_search: 1,
+        last_group: 1,
+        template: 1,
+        last_search_type: "Store",
+        last_route: "sales",
+      },
+    ],
+  },
+};
+
+export const userPrefsResp1 = {
+  data: {
+    error: 0,
+    success: true,
+    prefs: [
+      {
+        userid: 1,
+        last_search: 1,
+        last_group: 1,
+        template: 1,
+        last_search_type: "1",
+        last_route: "sales",
+      },
+    ],
+  },
+};
+export const userPrefsResp2 = {
+  data: {
+    error: 0,
+    success: true,
+    prefs: [
+      {
+        userid: 1,
+        last_search: 1,
+        last_group: 1,
+        template: 1,
+        last_search_type: "2",
+        last_route: "sales",
+      },
+    ],
+  },
+};
+export const userPrefsResp3 = {
+  data: {
+    error: 0,
+    success: true,
+    prefs: [
+      {
+        userid: 1,
+        last_search: 1,
+        last_group: 2,
+        template: 1,
+        last_search_type: "3",
+        last_route: "sales",
+      },
+    ],
+  },
+};
+
+export const userPrefsResp4 = {
+  data: {
+    error: 0,
+    success: true,
+    prefs: [
+      {
+        userid: 1,
+        last_search: 0,
+        last_group: 0,
+        template: 1,
+        last_search_type: "",
+        last_route: "",
+      },
+    ],
+  },
+};
+
+export const userStoresResp = {
+  data: {
+    error: 0,
+    success: true,
+    all_stores_for_user: [
+      {
+        storeid: 1,
+        store_number: "1",
+        store_name: "Store 1",
+      },
+    ],
+    assigned_stores: [
+      {
+        storeid: 1,
+        store_number: "1",
+        store_name: "Store 1",
+      },
+      {
+        storeid: 2,
+        store_number: "2",
+        store_name: "Store 2",
+      },
+      {
+        storeid: 3,
+        store_number: "3",
+        store_name: "Store 3",
+      },
+      {
+        storeid: 4,
+        store_number: "4",
+        store_name: "Store 4",
+      },
+    ],
+    unassigned_stores: [
+      {
+        storeid: 5,
+        store_number: "5",
+        store_name: "Store 5",
+      },
+      {
+        storeid: 6,
+        store_number: "6",
+        store_name: "Store 6",
+      },
+    ],
+  },
+};
+
+export const getGroupsResp = {
+  data: {
+    error: 0,
+    success: true,
+    msg: "Success",
+    groups: [
+      {
+        id: 1,
+        userid: 0,
+        group_name: "Test Group",
+      },
+      {
+        id: 2,
+        userid: 1,
+        group_name: "Admins",
+      },
+      {
+        id: 3,
+        userid: 0,
+        group_name: "Managers",
+      },
+      {
+        id: 4,
+        userid: 1,
+        group_name: "Random Group",
+      },
+    ],
+  },
+};
