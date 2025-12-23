94165ddfa95039ca928aa6541ea28d70a154f9dc
Stephen <stephenn649@gmail.com>
2025-12-23 09:15:18 -0600

    UpcUploader for Forecasting works, just waiting to plug it into Forecast until we get the work finished for the page and calculations

--- src/pages/forecast/components/UpcUploader.tsx
+++ src/pages/forecast/components/UpcUploader.tsx
@@ -1,13 +1,12 @@
 import { useState } from "react";
 import { useAppDispatch, useAppSelector } from "../../../hooks";
-
 import { useToast } from "../../../components/toasts/hooks/useToast";
 import {
   setUpcText,
   removeSingleUpc,
   setUpcs,
 } from "../../../features/upcUploadSlice";
-import FileInput from "../controls/FileInput";
+
 import { useForecastContext } from "../../../pages/forecast/hooks";
 import {
   reQuery,
@@ -18,8 +17,8 @@ import {
 } from "../../../features/forecastSlice";
 import { getHistoryFromList } from "../../../api/priceSim";
 import type { JsonError, PriceHistoryFromListResp } from "../../../interfaces";
-import { fitLinearDemand, predictQty } from "../utils";
-import { forecastUnits } from "../../priceSimulator/calc";
+import { formatRowData } from ".";
+import FileInput from "../controls/FileInput";
 
 const UpcUploader = () => {
   const toast = useToast();
@@ -63,44 +62,7 @@ const UpcUploader = () => {
           dispatch(setForecastResults(j.results));
 
           // set the row data
-          const rowData = j.results.map((item) => {
-            const prices = item.price_history.map((p) => [
-              parseFloat(p.price),
-              p.qty,
-            ]);
-
-            const linear = fitLinearDemand(prices);
-            const predictedQty = predictQty(prices[0][0], linear, prices);
-            const price = prices[0][0];
-            const daysAtPrice = item.price_history.find(
-              (p) => parseFloat(p.price) === price
-            )!.days_active;
-
-            const units = forecastUnits(
-              price, // 9.99
-              item.qty, // overall units sold in period
-              predictedQty, // 120 => from last 90 days
-              item.days_active, // total selling days
-              90, // on 90 day period
-              daysAtPrice, // days at price => based on the price history => 3
-              7, // forecastwindow
-              prices // price history to get qty at price
-              // adDays === undefined
-            );
-
-            return {
-              upc: item.upc,
-              description: item.description,
-              qtySold: prices[0][1],
-              daysActive: item.days_active, // active total
-              daysAtPrice: daysAtPrice, // active days at price
-              adFcst: units,
-              fcstPrice: price,
-              fcstTotal: price * units,
-              forecastWindow: 7,
-              adDays: 0, // this show as "" for 0 until user input
-            };
-          });
+          const rowData = formatRowData(j.results);
           dispatch(setRowData(rowData));
         }
       })
