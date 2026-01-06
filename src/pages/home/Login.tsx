671ec0f9447c94990a7f624bc5ac69a09e112bfc
Stephen <stephenn649@gmail.com>
2026-01-06 15:45:03 -0600

    Updates the last updated message in Login.tsx

--- src/pages/home/Login.tsx
+++ src/pages/home/Login.tsx
@@ -189,7 +189,7 @@ const Login = () => {
         </div>
         {/* Change this before pushing for publishing */}
         <div className="absolute bottom-1 left-0 text-sm pl-2">
-          Last Updated on 1/6/2026 @ 9:29 AM CST
+          Last Updated on 1/6/2026 @ 3:44 PM CST
         </div>
       </div>
       <div className="relative hidden w-0 flex-1 lg:block">
