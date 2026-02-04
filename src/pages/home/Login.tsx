25151ad68979a75cd1b01750655078e59c99e44a
Stephen <stephenn649@gmail.com>
2026-02-04 13:21:44 -0600

    updates the last updated message in Login.tsx

--- src/pages/home/Login.tsx
+++ src/pages/home/Login.tsx
@@ -229,7 +229,7 @@ const Login = () => {
         </div>
         {/* Change this before pushing for publishing */}
         <div className="absolute bottom-1 left-0 text-sm pl-2">
-          Last Updated on 2/3/2026 @ 4:16 PM CST
+          Last Updated 2/4/2026 @ 1:21 PM CST
         </div>
         {context.fetchingCredentials ? (
           <div className="absolute bottom-20 left-48 -pr-4 lg:p-0 lg:bottom-40 lg:left-72">
