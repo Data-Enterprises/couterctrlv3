f3946c3ab33a89480a5765be6e354e5132bab86d
Stephen <stephenn649@gmail.com>
2026-02-13 14:32:10 -0600

    Sets up the switch case in AdminPanels.tsx to render the selected module from the panels dropdown options in the admin controls

--- src/pages/admin/panels/AdminPanels.tsx
+++ src/pages/admin/panels/AdminPanels.tsx
@@ -1,10 +1,22 @@
 import { useAdminContext } from "../hooks";
-// import 
+import UserAdminPanel from "./UserAdminPanel";
+import CompanyAdminPanel from "./CompanyAdminPanel";
+import StoreAdminPanel from "./StoreAdminPanel";
+import VendorAdminPanel from "./VendorAdminPanel";
 
 const AdminPanels = () => {
   const { adminOption } = useAdminContext();
 
-  return <div className="bg-custom-white">Admin Panels</div>;
+  switch (adminOption) {
+    case 1:
+      return <UserAdminPanel />;
+    case 2:
+      return <CompanyAdminPanel />;
+    case 3:
+      return <StoreAdminPanel />;
+    case 4:
+      return <VendorAdminPanel />;
+  }
 };
 
 export default AdminPanels;
