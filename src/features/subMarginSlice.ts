ecf55f8587211271532fe17002717ac18a5d935c
Stephen <stephenn649@gmail.com>
2026-02-26 08:56:51 -0600

    Adds weeks 1 - 4 for weekly margin trends

--- src/features/subMarginSlice.ts
+++ src/features/subMarginSlice.ts
@@ -4,6 +4,10 @@ import type { SubDeptMargin, SubDept } from "../interfaces";
 interface SubMarginState {
   subDepts: SubDept[];
   margins: SubDeptMargin[];
+  weekOneMargins: SubDeptMargin[];
+  weekTwoMargins: SubDeptMargin[];
+  weekThreeMargins: SubDeptMargin[];
+  weekFourMargins: SubDeptMargin[];
   filteredMargins: SubDeptMargin[];
   selectedSubDeptId: number;
 }
@@ -11,6 +15,10 @@ interface SubMarginState {
 const initialState: SubMarginState = {
   subDepts: [],
   margins: [],
+  weekOneMargins: [],
+  weekTwoMargins: [],
+  weekThreeMargins: [],
+  weekFourMargins: [],
   filteredMargins: [],
   selectedSubDeptId: 0,
 };
@@ -31,15 +39,38 @@ const subMarginSlice = createSlice({
     setSelectedSubDeptId: (state, action: PayloadAction<number>) => {
       state.selectedSubDeptId = action.payload;
     },
+    setWeekTrendMargins: (
+      state,
+      action: PayloadAction<{ data: SubDeptMargin[]; week: number }>,
+    ) => {
+      // This reducer will always have a number between 1 and 4 for the week
+      // This will be used to display weekly sub dept margin trends
+      const { data, week } = action.payload;
+      switch (week) {
+        case 1:
+          state.weekOneMargins = data;
+          break;
+        case 2:
+          state.weekTwoMargins = data;
+          break;
+        case 3:
+          state.weekThreeMargins = data;
+          break;
+        case 4:
+          state.weekFourMargins = data;
+          break;
+      }
+    },
     resetSubMarginState: () => initialState,
   },
 });
 
 export const {
-  setSubDepts,
-  setMargins,
   setFilteredMargins,
+  setMargins,
   setSelectedSubDeptId,
+  setSubDepts,
+  setWeekTrendMargins,
   resetSubMarginState,
 } = subMarginSlice.actions;
 export default subMarginSlice.reducer;
