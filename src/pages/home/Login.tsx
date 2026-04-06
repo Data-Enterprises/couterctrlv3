8a338750a18d48d3a24b76b845839f670a1b71e6
Stephen <stephenn649@gmail.com>
2026-04-06 09:02:12 -0500

    Polishes onClick behavior for the store and cashier cards in the Cashiers page

--- src/pages/home/Login.tsx
+++ src/pages/home/Login.tsx
@@ -236,7 +236,7 @@ const Login = () => {
         </div>
         {/* TODO: Update this before pushing to publish */}
         <div className="absolute bottom-1 left-0 text-sm pl-2 select-none">
-          Last Updated 4/1/2026 @ 4:26 PM CST
+          Last Updated 4/6/2026 @ 9:01 AM CST
         </div>
       </div>
       <div className="relative hidden w-0 flex-1 lg:block">
