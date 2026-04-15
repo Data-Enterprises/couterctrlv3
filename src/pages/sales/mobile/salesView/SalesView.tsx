df6a3f8fe66495a673b158c637648a635d216919
Stephen <stephenn649@gmail.com>
2026-04-15 13:54:47 -0500

    Sets up the Weekly sales chart in Sales mobile UI for the inner sales view module

--- src/pages/sales/mobile/salesView/SalesView.tsx
+++ src/pages/sales/mobile/salesView/SalesView.tsx
@@ -6,19 +6,55 @@ import SalesViewWeekly from "./SalesViewWeekly";
 
 /**
  * THE SCROLL FLOW:
- * 
+ *
  * General overview => Granular
  * weekly => hourly => top ten
  */
 const SalesView = () => {
-  const { salesViewHourly, salesViewTopTen, salesViewWeekly } =
-    useMobileSalesCtx();
+  const {
+    salesViewHourly,
+    salesViewTopTen,
+    salesViewWeekly,
+    selectedStore,
+    groups,
+    assignedStores,
+    searchValue,
+    type,
+  } = useMobileSalesCtx();
+
+  const displayName = () => {
+    if (selectedStore.store_name.length > 0) return selectedStore.store_name;
+
+    if (type === "Group") {
+      const group = groups.find((g) => g.id === searchValue);
+      if (group) return group.group_name;
+    } else {
+      const store = assignedStores.find((s) => s.storeid === searchValue);
+      if (store) return store.store_name;
+    }
+
+    return "";
+  };
 
   return (
-    <div className="p-2">
-      <SalesViewWeekly weekly={salesViewWeekly} />
-      <SalesViewHourly hourly={salesViewHourly} />
-      <SalesViewTopTen topTen={salesViewTopTen} />
+    <div className="p-2 space-y-2">
+      <div className="bg-custom-white rounded-lg shadow-md px-2 py-0.5">
+        <div className="flex justify-between font-medium">
+          <div>Weekly Sales</div>
+          <div>{displayName()}</div>
+        </div>
+        <div className="grid grid-cols-2 mb-1">
+          <div className="bg-gradient-to-r from-blue-200 to-custom-white h-[1.5px]"></div>
+          <div className="bg-gradient-to-l from-blue-200 to-custom-white h-[1.5px]"></div>
+        </div>
+        <SalesViewWeekly weekly={[...salesViewWeekly]} />
+      </div>
+      <div className="bg-custom-white rounded-lg shadow-md px-2 py-0.5">
+        <SalesViewHourly hourly={salesViewHourly} />
+      </div>
+      <div className="bg-custom-white rounded-lg shadow-md px-2 py-0.5">
+        <SalesViewTopTen topTen={salesViewTopTen} />
+      </div>
     </div>
   );
 };
--- src/pages/sales/mobile/salesView/SalesViewWeekly.tsx
+++ src/pages/sales/mobile/salesView/SalesViewWeekly.tsx
@@ -1,17 +1,96 @@
 import type { PieData } from "..";
+import { ResponsiveBar } from "@nivo/bar";
+import { formatCurrency2 } from "../../../../utils";
 
 interface SalesViewWeeklyProps {
   weekly: PieData[];
 }
 
+const colors = [
+  "#00CC55",
+  "#0099AA",
+  "#0066FF",
+  "#3b82f6",
+  "#6688FF",
+  "#FF9900",
+  "#CC8844",
+];
+
 const SalesViewWeekly = ({ weekly }: SalesViewWeeklyProps) => {
-  console.log(weekly);
+  const formatId = (id: string) => {
+    return id.split("/").slice(0, 2).join("/");
+  };
+
+  const rgbaColor = (hex: string, alpha: number) => {
+    const r = parseInt(hex.slice(1, 3), 16);
+    const g = parseInt(hex.slice(3, 5), 16);
+    const b = parseInt(hex.slice(5, 7), 16);
+    return `rgba(${r},${g},${b},${alpha})`;
+  };
 
   return (
-    <div>
-      <div>Sales View - Weekly</div>
+    <div className="grid grid-cols-[30.5%_69.5%]">
+      <div>
+        {weekly.map((item, i) => (
+          <div key={i} className="flex gap-1 items-center">
+            <div
+              className="h-3 w-3 rounded-full"
+              style={{ backgroundColor: colors[i % colors.length] }}
+            ></div>
+            <div className="font-medium">{formatCurrency2(item.value)}</div>
+          </div>
+        ))}
+      </div>
+      <div className="h-full">
+        <ResponsiveBar
+          data={weekly}
+          margin={{ top: -5, right: 0, bottom: 30, left: 0 }}
+          enableLabel={false}
+          colors={(d) => rgbaColor(colors[d.index % colors.length], 0.3)}
+          padding={0.1}
+          borderRadius={4}
+          borderWidth={2}
+          axisLeft={null}
+          axisBottom={{
+            renderTick: ({ x, y, textX, textY, value }) => {
+              const dow = new Date(value as string)
+                .toDateString()
+                .split(" ")[0];
+              return (
+                <g transform={`translate(${x},${y + 4})`}>
+                  <line
+                    x1={0}
+                    y1={-4}
+                    x2={0}
+                    y2={1.5}
+                    stroke="black"
+                    strokeWidth={0.5}
+                  />
+                  <text
+                    textAnchor={"middle"}
+                    transform={`translate(${textX},${textY + 2})`}
+                    style={{
+                      fontSize: 10.5,
+                      fontWeight: "bolder",
+                      fontFamily: "Arial",
+                    }}
+                  >
+                    <tspan x={0} dy={0}>
+                      {dow}
+                    </tspan>
+                    <tspan x={0} dy={typeof value === "number" ? 4 : 12}>
+                      {formatId(value as string)}
+                    </tspan>
+                  </text>
+                </g>
+              );
+            },
+          }}
+          borderColor={(d) => rgbaColor(colors[d.index % colors.length], 1)}
+        />
+      </div>
     </div>
-  )
+  );
 };
 
 export default SalesViewWeekly;
