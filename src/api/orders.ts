d6b3ceeee5c6fce482ba0e337e6d9c01cd68eede
Stephen <stephenn649@gmail.com>
2026-04-20 13:07:24 -0500

    Sets up the axios calls for the new orders endpoints

---
+++ src/api/orders.ts
@@ -0,0 +1,49 @@
+import axios from "axios";
+
+export const getAvailableOrders = async (
+  url: string,
+  token: string,
+  start_date: string,
+  end_date: string,
+  storeids: number[],
+) => {
+  const json = await axios({
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    url: url + "orders/available_order",
+    data: {
+      start_date,
+      end_date,
+      storeids,
+    },
+  });
+
+  return json;
+};
+
+export const getAllOrders = async (
+  url: string,
+  token: string,
+  start_date: string,
+  end_date: string,
+  storeids: number[],
+) => {
+  const json = await axios({
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    url: url + "orders/all_orders",
+    data: {
+      start_date,
+      end_date,
+      storeids,
+    },
+  });
+
+  return json;
+};
