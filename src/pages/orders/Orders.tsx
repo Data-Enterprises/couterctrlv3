fb81b319350a60a142f1b9670d6399c471af55ba
Stephen <stephenn649@gmail.com>
2026-04-20 13:15:48 -0500

    Sets up the landing page for Orders

---
+++ src/pages/orders/Orders.tsx
@@ -0,0 +1,11 @@
+
+const Orders = () => {
+
+  return (
+    <div className="min-h-[calc(100vh-3rem)] max-h-[calc(100vh-3rem)] overflow-hidden p-4">
+      <div>Orders Page</div>
+    </div>
+  );
+};
+
+export default Orders;
