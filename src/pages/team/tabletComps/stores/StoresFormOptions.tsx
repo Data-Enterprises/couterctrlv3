cfcf8adc65c4863488681640961c3f64561a10c2
Stephen <stephenn649@gmail.com>
2026-05-08 10:08:19 -0500

    Sets up the start of the Team page stores forms and inner nav for tablet view

---
+++ src/pages/team/tabletComps/stores/StoresFormOptions.tsx
@@ -0,0 +1,71 @@
+import { useAppDispatch, useAppSelector } from "../../../../hooks";
+import {
+  setStoresFormOption,
+  setSelectedForm,
+} from "../../../../features/usersSlice";
+
+import {
+  IdentificationIcon,
+  ArrowLeftIcon,
+  UserIcon,
+  UserGroupIcon,
+} from "@heroicons/react/24/solid";
+
+const StoresFormOptions = () => {
+  const dispatch = useAppDispatch();
+  const { storesOption } = useAppSelector((state) => state.users);
+
+  const handleInnerNav = (form: string) => {
+    switch (form) {
+      case "assign":
+        dispatch(setStoresFormOption("assign"));
+        break;
+      case "info":
+        dispatch(setStoresFormOption("info"));
+        break;
+      case "bg_assign":
+        dispatch(setStoresFormOption("bg_assign"));
+        break;
+      default:
+        dispatch(setStoresFormOption(""));
+        dispatch(setSelectedForm(0));
+    }
+  };
+
+  return (
+    <div>
+      <div className="grid gap-3">
+        <div
+          className={`p-3 bg-custom-white rounded-lg flex flex-col justify-center items-center`}
+          onClick={() => handleInnerNav("")}
+        >
+          <ArrowLeftIcon className="w-12 h-12" />
+          <div>Go Back</div>
+        </div>
+        <div
+          className={`p-3 transition-all duration-200 ${storesOption === "assign" ? "bg-orange-200" : "bg-custom-white"} rounded-lg flex flex-col justify-center items-center`}
+          onClick={() => handleInnerNav("assign")}
+        >
+          <UserIcon className="w-12 h-12" />
+          <div>User Stores</div>
+        </div>
+        <div
+          className={`p-3 transition-all duration-200 ${storesOption === "info" ? "bg-orange-200" : "bg-custom-white"} rounded-lg flex flex-col justify-center items-center`}
+          onClick={() => handleInnerNav("info")}
+        >
+          <IdentificationIcon className="w-12 h-12" />
+          <div>Stores Info</div>
+        </div>
+        <div
+          className={`p-3 transition-all duration-200 ${storesOption === "bg_assign" ? "bg-orange-200" : "bg-custom-white"} rounded-lg flex flex-col justify-center items-center`}
+          onClick={() => handleInnerNav("bg_assign")}
+        >
+          <UserGroupIcon className="w-12 h-12" />
+          <div>Base Group Stores</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default StoresFormOptions;
--- src/pages/team/tabletComps/stores/StoresTablet.tsx
+++ src/pages/team/tabletComps/stores/StoresTablet.tsx
@@ -1,7 +1,41 @@
+import { useEffect } from "react";
+import { useAppDispatch, useAppSelector } from "../../../../hooks";
+import StoresFormOptions from "./StoresFormOptions";
+
+import {
+  resetUserInfo,
+  setSelectedUserId,
+  setStoresFormOption,
+} from "../../../../features/usersSlice";
+
 const StoresTablet = () => {
+  const dispatch = useAppDispatch();
+  const { storesOption } = useAppSelector((state) => state.users);
+
+  useEffect(() => {
+    return () => {
+      dispatch(setStoresFormOption(""));
+      dispatch(resetUserInfo());
+      dispatch(setSelectedUserId(0));
+    };
+  }, []);
+
+  const renderStoresForm = () => {
+    switch (storesOption) {
+      case "assign":
+        return <div>Create Store Form</div>;
+      case "info":
+        return <div>Delete Store Form</div>;
+      case "bg_assign":
+        return <div>Update Store Form</div>;
+      default:
+        return null;
+    }
+  };
   return (
-    <div>
-      <div>Stores</div>
+    <div className=" grid grid-cols-[17%_81.8%] gap-3">
+      <StoresFormOptions />
+      {renderStoresForm()}
     </div>
   );
 };
